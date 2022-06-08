e4c62def01cda06d37e6e6225d8b868307b47a70
hansen1416 <badapplesweetie@gmail.com>
2022-06-08 21:37:32 +0800

    1

--- kore/main.py
+++ kore/main.py
@@ -7,11 +7,7 @@ KAGGLE_AGENT_PATH = "/kaggle_simulations/agent/"
 if os.path.exists(KAGGLE_AGENT_PATH):
     # We're in the kaggle target system
     sys.path.insert(0, os.path.join(KAGGLE_AGENT_PATH, 'lib'))
-    agent_path = os.path.join(KAGGLE_AGENT_PATH, 'models', 'baseline_agent')
-else:
-    # We're somewhere else
-    sys.path.insert(0, os.path.join(os.getcwd(), 'lib'))
-    agent_path = os.path.join('models', 'baseline_agent')
+    agent_path = os.path.join(KAGGLE_AGENT_PATH, 'baseline_agent')
 
 from stable_baselines3 import PPO  # nopep8
 from environment import KoreGymEnv  # nopep8
@@ -28,11 +24,11 @@ def agent(obs, config):
     return kore_env.gym_to_kore_action(action)
 
 
-if __name__ == "__main__":
-    # This is for debugging purposes only before submitting - Are there any errors?
-    from kaggle_environments import make
-    from config import OPPONENT
-    env = make("kore_fleets", debug=True)
-    env.run(['main.py', OPPONENT])
+# if __name__ == "__main__":
+#     # This is for debugging purposes only before submitting - Are there any errors?
+#     from kaggle_environments import make
+#     from config import OPPONENT
+#     env = make("kore_fleets", debug=True)
+#     env.run(['main.py', OPPONENT])
 
-    print(env)
+#     print(env)
