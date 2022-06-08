198f1dc160da58f82d61a003f2a1349329a94da4
hansen1416 <badapplesweetie@gmail.com>
2022-06-08 17:22:51 +0800

    1

--- kore/main.py
+++ kore/main.py
@@ -1,51 +1,38 @@
-from kaggle_environments.envs.kore_fleets.helpers import *
-from random import randint
+# All this syspath wranglig is needed to make sure that the agent runs on the target environment and can load both the external dependencies
+# and the saved model. Dear kaggle, if possible, please make this easier!
+import os
+import sys
 
-# a flight plan
+KAGGLE_AGENT_PATH = "/kaggle_simulations/agent/"
+if os.path.exists(KAGGLE_AGENT_PATH):
+    # We're in the kaggle target system
+    sys.path.insert(0, os.path.join(KAGGLE_AGENT_PATH, 'lib'))
+    agent_path = os.path.join(KAGGLE_AGENT_PATH, 'models', 'baseline_agent')
+else:
+    # We're somewhere else
+    sys.path.insert(0, os.path.join(os.getcwd(), 'lib'))
+    agent_path = os.path.join('models', 'baseline_agent')
 
+from stable_baselines3 import PPO  # nopep8
+from environment import KoreGymEnv  # nopep8
+# Now for the actual agent
 
-def build_flight_plan(dir_idx, size):
-    flight_plan = ""
-    for i in range(4):
-        flight_plan += Direction.from_index((dir_idx + i) % 4).to_char()
-        if not i == 3:
-            flight_plan += str(size)
-    return flight_plan
+model = PPO.load(agent_path)
+kore_env = KoreGymEnv()
 
 
 def agent(obs, config):
-    board = Board(obs, config)
-    me = board.current_player
-
-    me = board.current_player
-    turn = board.step
-    spawn_cost = board.configuration.spawn_cost
-    kore_left = me.kore
-
-    for shipyard in me.shipyards:
-        if shipyard.ship_count >= 50:
-            flight_plan = build_flight_plan(randint(0, 3), randint(2, 9))
-            action = ShipyardAction.launch_fleet_with_flight_plan(
-                50, flight_plan)
-            shipyard.next_action = action
-        elif kore_left >= spawn_cost:
-            action = ShipyardAction.spawn_ships(1)
-            shipyard.next_action = action
-
-    return me.next_actions
+    kore_env.raw_obs = obs
+    state = kore_env.obs_as_gym_state
+    action, _ = model.predict(state)
+    return kore_env.gym_to_kore_action(action)
 
 
 if __name__ == "__main__":
-    import os
-    import sys
-    KAGGLE_AGENT_PATH = "/kaggle_simulations/agent/"
-    if os.path.exists(KAGGLE_AGENT_PATH):
-        sys.path.insert(0, os.path.join(KAGGLE_AGENT_PATH, 'lib'))
-    else:
-        sys.path.insert(0, os.path.join(os.getcwd(), 'lib'))
+    # This is for debugging purposes only before submitting - Are there any errors?
+    from kaggle_environments import make
+    from config import OPPONENT
+    env = make("kore_fleets", debug=True)
+    env.run(['main.py', OPPONENT])
 
-    from stable_baselines3.common.env_checker import check_env
-    from environment import KoreGymEnv
-
-    env = KoreGymEnv()
-    check_env(env)
+    print(env)
---
+++ kore/pack.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+tar -czf submission.tar.gz main.py config.py environment.py reward_utils.py models/baseline_agent.zip lib
